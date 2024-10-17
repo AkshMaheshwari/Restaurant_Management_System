13b058d82b817de69c4a2b545c3f474787dbf96b
Aksh Maheshwari <157402019+AkshMaheshwari@users.noreply.github.com>
2024-10-17 18:22:49 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,40 +4,40 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Order order = new Order();
-        
-        System.out.println("Welcome to the Multi-Cuisine Restaurant!");
-        System.out.println("Please choose a cuisine:");
-        System.out.println("1. Rajasthani");
-        System.out.println("2. Tamil Nadu");
-        System.out.println("3. Maharashtran");
-        System.out.println("4. Punjabi");
-
-        int cuisineChoice = sc.nextInt();
-        cuisine selectedCuisine;
-
-        // Determine which cuisine the user selected
-        switch (cuisineChoice) {
-            case 1:
-                selectedCuisine = new Rajasthani();
-                break;
-            case 2:
-                selectedCuisine = new TamilNadu();
-                break;
-            case 3:
-                selectedCuisine = new Maharastran();
-                break;
-            case 4:
-                selectedCuisine = new Punjabi();
-                break;
-            default:
-                System.out.println("Invalid choice. Exiting...");
-                return;
-        }
 
         boolean keepOrdering = true;
 
-        // Start the ordering process
         while (keepOrdering) {
+            System.out.println("\nWelcome to the Multi-Cuisine Restaurant!");
+            System.out.println("Please choose a cuisine:");
+            System.out.println("1. Rajasthani");
+            System.out.println("2. Tamil Nadu");
+            System.out.println("3. Maharashtran");
+            System.out.println("4. Punjabi");
+
+            int cuisineChoice = sc.nextInt();
+            cuisine selectedCuisine;
+
+            // Determine which cuisine the user selected
+            switch (cuisineChoice) {
+                case 1:
+                    selectedCuisine = new Rajasthani();
+                    break;
+                case 2:
+                    selectedCuisine = new TamilNadu();
+                    break;
+                case 3:
+                    selectedCuisine = new Maharastran();
+                    break;
+                case 4:
+                    selectedCuisine = new Punjabi();
+                    break;
+                default:
+                    System.out.println("Invalid choice. Exiting...");
+                    return;
+            }
+
+            // Take order for the selected cuisine
             Place_Order.takeOrder(sc, selectedCuisine, order);
 
             System.out.println("\nWould you like to order more? (yes/no)");
@@ -51,4 +51,3 @@ public class Main {
         sc.close();
     }
 }
-
